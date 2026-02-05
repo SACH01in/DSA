7c425b30a7b4b43407a6ca54f0c2f304b8856225
Sachin <72294882+SACH01in@users.noreply.github.com>
2026-02-06 01:22:13 +0530

    Time: 7 ms (17.94%) | Memory: 12.7 MB (38.72%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        pairs = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+
+        for ch in s:
+            if ch in pairs.values():   # opening bracket
+                stack.append(ch)
+            else:                      # closing bracket
+                if not stack or stack[-1] != pairs[ch]:
+                    return False
+                stack.pop()
+
+        return len(stack) == 0
