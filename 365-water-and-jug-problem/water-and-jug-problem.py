4d1c823824030eebdbdd3107b1400cbdee0aeff3
Sachin <72294882+SACH01in@users.noreply.github.com>
2026-02-05 22:18:32 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (93.89%) - LeetSync

---
+++ 365-water-and-jug-problem/water-and-jug-problem.py
@@ -0,0 +1,23 @@
+class Solution(object):
+
+    def canMeasureWater(self, x, y, target):
+        """
+        :type x: int
+        :type y: int
+        :type target: int
+        :rtype: bool
+        """
+        def GCD(a,b):
+            while b>0:
+                [a,b] = [b,a%b]
+            return a
+
+        if(target > x+y):
+            return False
+        elif (target == x+y):
+            return True
+        elif (target % GCD(x,y) == 0):
+            return True
+
+        return False
+        
