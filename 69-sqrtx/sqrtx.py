3fe5ff8da609f5bb7d064bd976de25cc4af9c1ad
Sachin <72294882+SACH01in@users.noreply.github.com>
2026-02-12 20:20:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (18.58%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -4,9 +4,24 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        if x<2 :
+        if x < 2:
             return x
-        i = 2
-        while i*i <= x :
-            i += 1
-        return i-1
+        
+        start = 1
+        end = x
+        ans = 0
+        
+        while start <= end:
+            mid = start + (end - start) // 2
+            
+            if mid * mid == x:
+                return mid
+            
+            elif mid * mid < x:
+                ans = mid
+                start = mid + 1
+            
+            else:
+                end = mid - 1
+        
+        return ans
